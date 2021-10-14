ebd9d163724622360976212400d489dfaa893ca5
Logan Rechler <43584963+LoganRechler@users.noreply.github.com>
2021-10-14 19:39:21 -0400

    Now scans ports

--- PortScanner.py
+++ PortScanner.py
@@ -1,7 +1,17 @@
 import pyfiglet
+import socket
 
-def scan(url):
-    return ""
+def pingScan(url):
+    address = socket.gethostbyname(url)
+    openPorts = [0]*65535
+    for port in range(1, 65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.004)
+        result = s.connect_ex((address, port))
+        if result == 0:
+            openPorts[port] = 1
+            print("Port {} is open".format(port))
+        s.close()
 
 if __name__ == "__main__":
     pyfiglet.print_figlet("Port Scanner")
@@ -9,4 +19,4 @@ if __name__ == "__main__":
     start = input("Start?: Y/N\n")
     if start == "Y" or start == "y" or start == "Yes" or start == "Yes":
         url = input("Enter a URL to scan:\n")
-        print(scan(url))
+        pingScan(url)
