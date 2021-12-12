e5fee4ad75a60f5e4a9b15c367007055a0a0d0ef
Logan Rechler <43584963+LoganRechler@users.noreply.github.com>
2021-12-12 15:28:33 -0500

    Add files via upload

--- PortScanner.py
+++ PortScanner.py
@@ -1,5 +1,13 @@
+
 import pyfiglet
 import socket
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from scapy.layers.inet import IP, TCP, ICMP
+from scapy.sendrecv import sr1
+from scapy.volatile import RandShort
+
 
 def pingScan(url):
     address = socket.gethostbyname(url)
@@ -13,10 +21,36 @@ def pingScan(url):
             print("Port {} is open".format(port))
         s.close()
 
+def tcpHalfOpenScan(url):
+
+    dst_ip = "10.0.0.1"
+    src_port = RandShort()
+    dst_port = 80
+    ip = IP(dst=dst_ip)
+    tcp = TCP(sport=src_port, dport=dst_port, flags="S")
+    stealth_scan_resp = sr1(ip / tcp, timeout = 10)
+    if (str(type(stealth_scan_resp)) ==" < type ‘NoneType’ > "):
+        print("Filtered")
+    elif(stealth_scan_resp.haslayer(TCP)):
+        if (stealth_scan_resp.getlayer(TCP).flags == 0x12):
+            send_rst = sr1(IP(dst=dst_ip) / TCP(sport=src_port, dport=dst_port, flags="R"), timeout = 10)
+            print("Open")
+        elif(stealth_scan_resp.getlayer(TCP).flags == 0x14):
+            print("Closed")
+        elif(stealth_scan_resp.haslayer(ICMP)):
+            if (int(stealth_scan_resp.getlayer(ICMP).type) == 3 and int(stealth_scan_resp.getlayer(ICMP).code) in [1, 2,
+                                                                                                                   3, 9,
+                                                                                                                   10,
+                                                                                                                   13]):
+                print("Filtered")
+
 if __name__ == "__main__":
-    pyfiglet.print_figlet("Port Scanner")
-    pyfiglet.print_figlet("Socket Science", font="bubble")
-    start = input("Start?: Y/N\n")
-    if start == "Y" or start == "y" or start == "Yes" or start == "Yes":
-        url = input("Enter a URL to scan:\n")
-        pingScan(url)
+    tcpHalfOpenScan("a")
+    # pyfiglet.print_figlet("Port Scanner")
+    # pyfiglet.print_figlet("Socket Science", font="bubble")
+    # start = input("Start?: Y/N\n")
+    # if start == "Y" or start == "y" or start == "Yes" or start == "Yes":
+    #     url = input("Enter a URL to scan:\n")
+    #     pingScan(url)
+
+
